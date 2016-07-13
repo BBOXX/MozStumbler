e748664a6dbacac410a98e9b564f2613e80808bc
Bryn Truscott <bryn.truscott@gmail.com>
2016-07-13 17:19:16 +0100

    Rebrand to distinguish from vanilla Mozilla version

--- rename_release.sh
+++ rename_release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # bad hack to rename the final apk
 mkdir -p outputs
-cmd="cp android/build/outputs/apk/MozStumbler-$1.apk outputs/MozStumbler-`git describe --abbrev=0 --tags`.apk"
+cmd="cp android/build/outputs/apk/MozStumbler-$1.apk outputs/BBOXXStumbler-`git describe --abbrev=0 --tags`.apk"
 echo $cmd
 `${cmd}`
